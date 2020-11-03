153952e4181c97ece7ee13aff72116ab203af660
HP <HP@DESKTOP-OHUQA4D>
2020-11-03 14:25:03 -0600

    ejemplo del lenguaje C

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: HP
+ *
+ * Created on 3 de noviembre de 2020, 02:04 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("hola mundo");
+    
+    return (EXIT_SUCCESS);
+}
+
