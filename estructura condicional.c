2d7846e70ac040e4ef5c82ffd332ec14989334bb
HP <HP@DESKTOP-OHUQA4D>
2020-11-09 11:48:39 -0600

    ejemplos al usar if

---
+++ estructura condicional.c
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   estructura condicional.c
+ * Author: HP
+ *
+ * Created on 9 de noviembre de 2020, 11:44 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    float x = 6;
+    float y = 17;
+    
+   if (x == y) {
+       printf ("son iguales");
+   } else{
+    printf ("Son diferentes");
+    return (0);
+}
+
+    return (EXIT_SUCCESS);
+}
+
