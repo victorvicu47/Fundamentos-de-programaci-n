4f115bcb307ac8de267e63cef960e765f4eeb0a6
HP <HP@DESKTOP-OHUQA4D>
2020-11-12 15:31:45 -0600

    Uso de while adinado para hacer tablas de multiplicar del 1 al 10

---
+++ tablas.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+    
+    int t = 1;
+    int j = 1;
+    while(t<=10){
+        j = 1;
+    while(j<=10){    
+    }
+        printf("%d X %d = %d\n",t,j, t*j);
+        j = j + 1;
+    }
+    
+    printf("-------\n");
+    
+    t = t + 1;
+      return (EXIT_SUCCESS);
+}
