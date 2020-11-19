2871afa9cc65b19c82566bd622d0f837dcbbaa26
HP <HP@DESKTOP-OHUQA4D>
2020-11-19 15:02:23 -0600

    Usando comando de arreglos simples

---
+++ Arreglos simples.c
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Arreglos simples.c
+ * Author: HP
+ *
+ * Created on 19 de noviembre de 2020, 02:56 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    int arreglo[5];
+    int x = 0;
+    
+    for (x;x<=5;x=x+1){
+        printf("hola");
+    }
+    return (0);
+}
+
