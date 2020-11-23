b55da6932ccd51da7c187657c799d01886d0ac4c
HP <HP@DESKTOP-OHUQA4D>
2020-11-23 00:36:09 -0600

    Usando comando de arreglos simples de 100 elementos

---
+++ Arreglos de 100 elementos.c
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Arreglos de 100 elementos.c
+ * Author: HP
+ *
+ * Created on 23 de noviembre de 2020, 12:04 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main() {
+int arreglo[100];
+    int x = 0;
+ for(x;x<=100;x=x+1){
+    printf("numero [%d] = ",x);
+ }
+    return 0;
+}
