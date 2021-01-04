efa9ab851264870647ef1e3597027b90aaaf08eb
HP <HP@DESKTOP-OHUQA4D>
2021-01-03 19:18:18 -0600

    Problema 2 del segundo tema de estructuras de control usando while

---
+++ while.c
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   while.c
+ * Author: HP
+ *
+ * Created on 10 de noviembre de 2020, 03:19 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    int x  = 1;
+    // 0,1,2,3,4,5
+    int t = 1;
+    int tope = 10;
+    
+    while(x<=tope){
+        printf("%d X %d = %d\n",t,x,t*x);
+        x = x + 1;
+       
+    }
+    
+    
+  
+   
+    // tarea hacer varias tablas
+    //con while pero anidado
+       
+    return (EXIT_SUCCESS);
+}
+
