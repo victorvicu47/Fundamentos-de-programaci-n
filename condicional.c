efd1ae6df4f7eff33a2c9f3826979cfc980e963e
HP <HP@DESKTOP-OHUQA4D>
2021-01-03 18:38:35 -0600

    Problema 1 del segundo tema de estructuras de control

---
+++ condicional.c
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   condicional.c
+ * Author: HP
+ *
+ * Created on 3 de enero de 2021, 06:34 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+  float x = 6;
+    float y = 17;
+    
+   if (x == y) {
+       printf ("son iguales");
+   } else{
+    printf ("Son diferentes");
+    return (0);
+}
+}
