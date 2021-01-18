b38eafba177db89e1497159ea2caf73903171cfa
HP <HP@DESKTOP-OHUQA4D>
2021-01-18 00:16:35 -0600

    Trabajo en clase

---
+++ ARRAYS clase.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	
+	int x[5] ={1,2,4,6,7};
+	int fila;
+	
+	for(fila = 0;fila < 5;fila++){
+		printf("%i ", x[fila]);
+	}
+	
+	return 0;
+}
