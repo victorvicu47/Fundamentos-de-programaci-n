9b791171a837be3a0dba475f88079cbe3b4d3176
HP <HP@DESKTOP-OHUQA4D>
2021-01-18 01:12:24 -0600

    Trabajo en clase cabecera

---
+++ Cabecera.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "cabecera.h"
+float altura = 30;
+
+int suma(int a, int b){
+    return a+b;
+}
+
+int resta(int a, int b){
+    return a-b;
+}
+
+char Primer_caracter(char* cadena){
+    return cadena[0];
+}
+
+double area_triangulo(int altura){
+    
+    return base*altura/2;
+}
+
