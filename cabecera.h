4e98310b36cd0c5248326042daf8c1e0bc839c3b
HP <HP@DESKTOP-OHUQA4D>
2021-01-18 01:11:16 -0600

    Trabajo en clase cabecera

---
+++ cabecera.h
@@ -0,0 +1,11 @@
+#ifndef CABECERAH
+#define CABECERAH
+#include <stdio.h>
+
+int suma(int a, int b);
+int resta(int a, int b);
+char Primer_caracter(char* cadena);
+double Area_triangulo(int altura);
+
+float altura;
+#endif
