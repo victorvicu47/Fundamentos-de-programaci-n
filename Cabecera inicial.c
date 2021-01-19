7551d288fbc6bbbc9b3f3c891d74ea3e298c983d
HP <HP@DESKTOP-OHUQA4D>
2021-01-19 00:47:32 -0600

    Corrección del trabajo en clase cabecera inicial

--- Cabecera inicial.c
+++ Cabecera inicial.c
@@ -4,20 +4,20 @@
 
 int main()
 {
-    int a=3, b=2, c;
+    int a = 3, b = 2, c;
     float area;
     char inicial;
-    
-    c=suma(a,b);
-    printf("c: %d/n",c);
-    c=resta(a,b);
-    printf("c: %d/n",c);
-    
-    area=area_triangulo(2);
-    printf("area; %f/n",area);
-    
-    inicial=Primer_caracter("chelin");
-    printf("Primer caracter %c",inicial);
+
+    c = suma (a,b);
+    printf("c: %d\n",c);
+    c = resta (a,b);
+    printf("c: %d\n",c);
+
+    area = area_circulo(2);
+    printf("area: %f\n",area);
+
+    inicial = primer_caracter("chelin");
+    printf("primer caracter:  %c",inicial);
+
     return 0;
 }
-
