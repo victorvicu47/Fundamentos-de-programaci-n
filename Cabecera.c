0fe113103b9026ac8b4d42657a5694cae22bf420
HP <HP@DESKTOP-OHUQA4D>
2021-01-19 00:48:42 -0600

    Corrección del trabajo en clase cabecera.c

--- Cabecera.c
+++ Cabecera.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include "cabecera.h"
-float altura = 30;
 
-int suma(int a, int b){
-    return a+b;
+
+float PI = 3.1452;
+
+int suma(int a, int b ) {
+        return a+b;
+
 }
 
-int resta(int a, int b){
-    return a-b;
+int resta ( int a, int b ){
+
+        return a-b;
+
 }
 
-char Primer_caracter(char* cadena){
-    return cadena[0];
+char primer_caracter(char* cadena) {
+        return cadena[0];
 }
 
-double area_triangulo(int altura){
-    
-    return base*altura/2;
-}
+double area_circulo(int radio) {
 
+        return PI * radio * radio;
+}
