d16d5a4dd412996c027a6243973f3401b3906cd5
HP <HP@DESKTOP-OHUQA4D>
2021-01-23 01:11:18 -0600

    Archivo usando codigo ACII

---
+++ Archivo de codigo ACCI.c
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Archivo de codigo ACCI.c
+ * Author: HP
+ *
+ * Created on 23 de enero de 2021, 01:09 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+main()
+{
+	unsigned char palabra[21];
+	char s,l;
+	int i,j;
+	l = 0;
+	printf("ingrese una palabra no mayor a 5 letras: ");
+	scanf("%c", &palabra[s]);
+	j = palabra[s];
+	for(int i = 5; i > 0; --i){
+	l = 0;
+	printf("El caracter %c equivale a %d en codigo ASCII\n", j, palabra[s]);
+}
+	getchar();
+	getchar();
+	return 0;
+}
